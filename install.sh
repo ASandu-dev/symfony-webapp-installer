10e45a0265af50ca96327408b3927a663c6a3b0d
Andrei Sandu <45360005+ASandu-dev@users.noreply.github.com>
2023-07-05 18:46:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,52 +1,83 @@
 #!/bin/bash
 
+# Install Composer
+if ! command -v composer &> /dev/null; then
+    sudo apt update
+    sudo apt install composer
+fi
+
+# Install Node.js
+if ! command -v node &> /dev/null; then
+    curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt update
+    sudo apt install -y nodejs
+fi
+
+# Install Symfony CLI
+if ! command -v symfony &> /dev/null; then
+    wget https://get.symfony.com/cli/installer -O - | bash
+    mv /home/$USER/.symfony5/bin/symfony /usr/local/bin/symfony
+fi
+
+# Install PHP
+if ! command -v php &> /dev/null; then
+    sudo apt update
+    sudo apt install -y php
+fi
+
+# Install phpenv
+if [ ! -d "$HOME/.phpenv" ]; then
+    git clone https://github.com/phpenv/phpenv.git ~/.phpenv
+    echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(phpenv init -)"' >> ~/.zshrc
+    source ~/.zshrc
+fi
+
+# Install PHP versions with phpenv
+php_versions=("7.4.1" "8.1.0" "8.2.7")
+for version in "${php_versions[@]}"; do
+    if [ ! -d "$HOME/.phpenv/versions/$version" ]; then
+        phpenv install $version
+    fi
+done
+
+# Set the latest PHP version as the global version
+latest_php_version=$(phpenv versions --bare | grep -v '-' | tail -1)
+phpenv global $latest_php_version
+
+# Install Visual Studio Code (code command)
+if ! command -v code &> /dev/null; then
+    curl -fsSL https://code.visualstudio.com/docs/?dv=linux64_deb | sudo -E bash -
+    sudo apt install -y ./code*.deb
+    rm ./code*.deb
+fi
+
 # Install Zsh
-sudo apt-get update
-sudo apt-get install zsh -y
+if ! command -v zsh &> /dev/null; then
+    sudo apt install -y zsh
+fi
 
-# Install Git and GitHub CLI
-sudo apt-get install git -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt-get update
-sudo apt-get install gh -y
+# Install Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
+
+# Add paths to the system
+echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.zshrc
+echo 'export PATH="$PATH:/usr/local/bin/symfony"' >> ~/.zshrc
+echo 'export PATH="$PATH:$HOME/.phpenv/bin"' >> ~/.zshrc
+echo 'eval "$(phpenv init -)"' >> ~/.zshrc
+echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.zshrc
 
 # Set Zsh as the default shell
 chsh -s $(which zsh)
 
-# Install Oh My Zsh (Optional, but recommended)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Symfony CLI
-wget https://get.symfony.com/cli/installer -O - | bash
-export PATH="$HOME/.symfony/bin:$PATH"
-
-# Install Composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-
-# Install PHP
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-sudo apt-get install php8.2 -y
-
-# Install PHP Env
-curl -L https://github.com/phpenv/phpenv-installer/raw/master/bin/phpenv-installer | bash
-echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(phpenv init -)"' >> ~/.zshrc
-
-# Install Node Env
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.zshrc
-
-# Source the .zshrc file to apply the changes immediately
+# Install Agnoster theme
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/agnoster" ]; then
+    git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git ~/.oh-my-zsh/custom/themes/agnoster
+fi
+# Reload the Zsh configuration
 source ~/.zshrc
 
-echo "Installation completed."
-```
-
-This modified script installs PHP version 8.2.7 by adding the Ondřej Surý PPA repository and updating the package list. It then installs PHP 8.2 using the `php8.2` package.
+echo "Setup completed
